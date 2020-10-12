3f6adc493f2e203f0bcee33ddc09541b2052f31b
jeongjaein <wodls96@gmail.com>
2020-10-12 21:20:45 +0900

    feat : LocationPicker Start

--- LocationPicker/ViewController.swift
+++ LocationPicker/ViewController.swift
@@ -6,12 +6,24 @@
 //
 
 import UIKit
+import NMapsMap
+import Then
 
 class ViewController: UIViewController {
-
+    var mapView = NMFMapView()
+    let pinImage = UIImageView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        attribute()
+        layout()
+    }
+    func attribute() {
+        mapView = NMFMapView(frame: view.frame)
+    }
+    
+    func layout() {
+        view.addSubview(mapView)
     }
 }
 
