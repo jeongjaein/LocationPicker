367eab01dcc2179e8968535259813492f3837eff
jeongjaein <wodls96@gmail.com>
2020-10-15 19:01:16 +0900

    feat : 키보드 숨기기, 해당위치로 이동 완성

--- LocationPicker/ViewController.swift
+++ LocationPicker/ViewController.swift
@@ -23,6 +23,9 @@ class ViewController: UIViewController {
     let decoder = JSONDecoder()
     var totalList: [NMGLatLng] = []
     var cameraUpdate = NMFCameraUpdate()
+    var newLng: Double = 0
+    var newLat: Double = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -83,6 +86,12 @@ class ViewController: UIViewController {
             $0.heightAnchor.constraint(equalToConstant: 50).isActive = true
         }
     }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+
+          self.view.endEditing(true)
+
+    }
+
     func search(keyword: String) -> String {
         let parameters: [String: String] = [
             "query": keyword
@@ -95,34 +104,14 @@ class ViewController: UIViewController {
                     case .success(let value):
                         if let addressList = JSON(value)["documents"].array {
                             for item in addressList {
-                                let lng = item["x"].string ?? ""
-                                let lat = item["y"].string ?? ""
-                                var test: NMFCameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: Double(lat)!, lng: Double(lng)!))
+                                newLng = Double(item["x"].string ?? "")!
+                                newLat = Double(item["y"].string ?? "")!
+                                var test: NMFCameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: newLat, lng: newLng))
                                 test.animation = .easeOut
                                 mapView.moveCamera(test)
                                 break
-                                //                                let addressName = item["address_name"].string ?? ""
-                                //                                let jibunAddress = item["address_name"].string ?? "없음"
-                                //                                let roadAddress = item["road_address"].string ?? "없음"
-                                //                                let depthOneName = item["address"]["region_1depth_name"].string ?? ""
-                                //                                let depthTwoName = item["address"]["region_2depth_name"].string ?? ""
-                                //                                let lng = item["address"]["x"].string ?? ""
-                                //                                let lat = item["address"]["y"].double ?? 0
-                                //                                let depthThreeName = item["address"]["region_3depth_name"].string ?? ""
-                                //                                let postCode = (item["address"]["zip_code"].string ?? "").isEmpty ? "우편번호 없음" : item["address"]["zip_code"].string ?? ""
-                                //                                totalList.append(Address(addressName: addressName,
-                                //                                                         postCode: postCode,
-                                //                                                         roadAddr: roadAddress,
-                                //                                                         jibunAddr: jibunAddress,
-                                //                                                         depthOneAddr: depthOneName,
-                                //                                                         deptTwoAddr: depthTwoName,
-                                //                                                         deptThreeAddr: depthThreeName,
-                                //                                                         lng: Double(lng)!,
-                                //                                                         lat: lat)
-                                //                                )
                             }
                         }
-                        print(totalList)
                     case .failure(let err) :
                         print(err)
                     }
@@ -136,6 +125,7 @@ extension ViewController: NMFMapViewCameraDelegate {
         task = DispatchWorkItem { [self] in
             self.pin.alpha = 1
             addressLabel.text = "\(mapView.cameraPosition.target.lat),   \(mapView.cameraPosition.target.lng)"
+            print(newLat,newLng)
             UIView.animate(withDuration: 0.25, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
                 self.pin.transform = CGAffineTransform(translationX: 0, y: 0)
             })
@@ -143,6 +133,7 @@ extension ViewController: NMFMapViewCameraDelegate {
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: task!)
     }
     func mapView(_ mapView: NMFMapView, cameraWillChangeByReason reason: Int, animated: Bool) {
+        self.view.endEditing(true)
         task?.cancel()
         pin.alpha = 0.5
         UIView.animate(withDuration: 0.25, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
@@ -153,7 +144,6 @@ extension ViewController: NMFMapViewCameraDelegate {
 extension ViewController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         if textField.text!.isEmpty {
-            print("검색어를 입력해주세요 ")
             return false
         }else {
             search(keyword: textField.text ?? "")
@@ -162,3 +152,25 @@ extension ViewController: UITextFieldDelegate {
     }
 }
 
+
+
+
+//                                let addressName = item["address_name"].string ?? ""
+//                                let jibunAddress = item["address_name"].string ?? "없음"
+//                                let roadAddress = item["road_address"].string ?? "없음"
+//                                let depthOneName = item["address"]["region_1depth_name"].string ?? ""
+//                                let depthTwoName = item["address"]["region_2depth_name"].string ?? ""
+//                                let lng = item["address"]["x"].string ?? ""
+//                                let lat = item["address"]["y"].double ?? 0
+//                                let depthThreeName = item["address"]["region_3depth_name"].string ?? ""
+//                                let postCode = (item["address"]["zip_code"].string ?? "").isEmpty ? "우편번호 없음" : item["address"]["zip_code"].string ?? ""
+//                                totalList.append(Address(addressName: addressName,
+//                                                         postCode: postCode,
+//                                                         roadAddr: roadAddress,
+//                                                         jibunAddr: jibunAddress,
+//                                                         depthOneAddr: depthOneName,
+//                                                         deptTwoAddr: depthTwoName,
+//                                                         deptThreeAddr: depthThreeName,
+//                                                         lng: Double(lng)!,
+//                                                         lat: lat)
+//                                )
